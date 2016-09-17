2b52ea9e475fb535946258aba4406d6f6a8d8d12
liheyi <liheyi.liheyuan@gmail.com>
2016-09-17 19:05:29 +0800

    add new code for sicp in 20160917

---
+++ programming/sicp/1.2/exponent2_by_recursion.scm
@@ -0,0 +1,10 @@
+(define (even n)
+  (= (remainder n 2) 0)
+)
+
+(define (fast_expt b n)
+  (cond ((= n 0) 1)
+        ((even n) (square (fast_expt b (/ n 2))))
+        (else (* b (fast_expt b (- n 1))))
+  )
+)
