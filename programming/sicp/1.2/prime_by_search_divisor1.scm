e3763abf87d06d4d830bcee37d3ca53789ae14a6
liheyi <liheyi.liheyuan@gmail.com>
2016-09-20 19:01:17 +0800

    add code for sicp

---
+++ programming/sicp/1.2/prime_by_search_divisor1.scm
@@ -0,0 +1,11 @@
+(define (prime n)
+
+  (define (find_divisor n test_divisor)
+    (define (divides a b) (= (remainder a b) 0))
+    (cond ((> (square test_divisor) n) n)
+          ((divides n test_divisor) test_divisor)
+          (else (find_divisor n (+ test_divisor 1)))))
+
+  (define (smallest_divisor n) (find_divisor n 2))
+
+  (= n (smallest_divisor n)))
