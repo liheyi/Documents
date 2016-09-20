a0b34af5d8991cd4b784aa2633a6f338c8e7ebb2
liheyi <liheyi.liheyuan@gmail.com>
2016-09-21 00:05:49 +0800

    add codes for sicp

---
+++ programming/sicp/1.2/search_for_primes2.scm
@@ -0,0 +1,27 @@
+(define (search_for_primes n)
+  (define (search_consecutive_prime n count)
+    (define (next_odd n)
+      (define (odd n) (= (remainder n 2) 1))
+      (if (odd n) (+ n 2) (+ n 1)))
+
+    (define (prime n)
+      (define (smallest_divisor n) 
+        (define (find_divisor n test_divisor)
+          (define (divides a b) (= (remainder a b) 0))
+          (cond ((> (square test_divisor) n) n)
+                ((divides n test_divisor) test_divisor)
+                (else (find_divisor n (next_odd test_divisor)))))
+          (find_divisor n 2))
+      (= n (smallest_divisor n)))
+
+	(cond ((= count 0) (display "are primes."))
+          ((prime n)
+           (display n)
+           (newline)
+           (search_consecutive_prime (next_odd n) (- count 1)))
+          (else (search_consecutive_prime (next_odd n) count))))
+
+
+  (let ((start_time (real-time-clock)))
+		(search_consecutive_prime n 3)
+	   (- (real-time-clock) start_time)))
--- programming/sicp/1.2/smallest_divisor.scm
+++ programming/sicp/1.2/smallest_divisor.scm
@@ -1,12 +1,10 @@
-(define (divides a b) (= (remainder a b) 0))
-
-(define (find_divisor n test_divisor)
-  (cond ((> (square test_divisor) n) n)
-        ((divides n test_divisor) test_divisor)
-        (else (find_divisor n (+ test_divisor 1)))
-  )
-)
-
 (define (smallest_divisor n) 
+
+  (define (find_divisor n test_divisor)
+    (define (divides a b) (= (remainder a b) 0))
+    (cond ((> (square test_divisor) n) n)
+          ((divides n test_divisor) test_divisor)
+          (else (find_divisor n (+ test_divisor 1)))))
+
   (find_divisor n 2)
 )
